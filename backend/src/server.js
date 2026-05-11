5cdfe9dc3f0bee5c972d52b7ee339b09aeffc81e
abhaypandey092004 <abhaypandey092004@gmail.com>
2026-05-11 10:04:14 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,13 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// });
+if (process.env.NODE_ENV !== "production") {
+  app.listen(process.env.PORT || 5000, () => {
+    console.log("Server running locally");
+  });
+}
+
+export default app;
